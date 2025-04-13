f5f46a563af8a7cfab0722e5902c20edd5b1f969
artiom mezheynikov <artiom.mezheynikov.work@gmail.com>
2025-04-13 11:18:22 +0300

    added mobile menu. adapted block empty

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const menuSwitcher = document.getElementById('menu-switch');
+const mobileMenu = document.querySelector('.mobile-menu__wrapper');
+
+
+// -----======For mobile menu ========----
+menuSwitcher.addEventListener('change', (e) => {
+	if (e.target.checked) {
+		bodyLock();
+		menuSwitcher.labels[0].title = 'close';
+	} else if (!e.target.checked) {
+		bodyUnlock();
+		menuSwitcher.labels[0].title = 'mobile menu';
+	}
+});
+mobileMenu.addEventListener('click', (e) => {
+	menuSwitcher.checked = false;
+	bodyUnlock();
+});
+
+function bodyLock() {
+	document.body.classList.add('lock-body');
+}
+function bodyUnlock() {
+	document.body.classList.remove('lock-body');
+}
