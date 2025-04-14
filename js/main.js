9414a139e94f6c4c5ba92d61e32bb222e37b80ac
artiom mezheynikov <artiom.mezheynikov.work@gmail.com>
2025-04-14 03:05:12 +0300

    added modal window

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const menuSwitcher = document.getElementById('menu-switch');
 const mobileMenu = document.querySelector('.mobile-menu__wrapper');
 
 
-// -----======For mobile menu ========----
+// -----====== For mobile menu ========----
 menuSwitcher.addEventListener('change', (e) => {
 	if (e.target.checked) {
 		bodyLock();
@@ -20,8 +20,21 @@ mobileMenu.addEventListener('click', (e) => {
 });
 
 function bodyLock() {
+	const lockPaddingValue = window.innerWidth - document.querySelector('body').offsetWidth + "px";
+	document.body.style.paddingRight = lockPaddingValue;
 	document.body.classList.add('lock-body');
 }
 function bodyUnlock() {
+	document.body.style.paddingRight = '0px';
 	document.body.classList.remove('lock-body');
 }
+
+// -----====== Codr for modal window ========----
+
+$("#new-rent").iziModal({
+	width: 1074,
+	padding: 0,
+	focusInput: false,
+	overlayColor: 'rgba(49, 47, 47, 0.5)',
+	bodyOverflow: true,
+});
